57d8fce66f5922dbdcf85b7ec8567c47fe7c373f
wintercode <kenfackjordanjunior@gmail.com>
2025-02-03 19:15:52 +0100

    implement package and subscription handling with CRUD operations; add PackageController and SubscriptionController for managing packages and subscriptions

---
+++ src/App/Controllers/PackageController.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+use App\Db\Repository\PackageService;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use Valitron\Validator;
+
+class PackageController
+{
+  public function __construct(private PackageService $packageService, private Validator $validator)
+  {
+    $this->validator->mapFieldsRules([
+      'title' => ['required', ['lengthMin', 4]],
+      'price' => ['required', ['lengthMin', 4]],
+      'user_id' => ['required', ['lengthMin', 1]],
+    ]);
+  }
+
+  public function showAll(Request $request, Response $response): Response
+  {
+    $data = $this->packageService->readAll();
+    $response->getBody()->write(json_encode($data));
+    return $response;
+  }
+
+  public function show(Request $request, Response $response, string $package_id): Response
+  {
+    $package = $request->getAttribute('package');
+    $response->getBody()->write(json_encode($package));
+    return $response;
+  }
+
+  public function create(Request $request, Response $response): Response
+  {
+    $data = $request->getParsedBody();
+
+    $this->validator = $this->validator->withData($data);
+    if (! $this->validator->validate()) {
+      $response->getBody()->write(json_encode($this->validator->errors()));
+      return $response->withStatus(422);
+    }
+
+    $user = $request->getAttribute('author');
+
+    $package = $this->packageService->create($user, $data);
+    $response->getBody()->write(json_encode($package));
+    return $response;
+  }
+
+  public function update(Request $request, Response $response, string $package_id): Response
+  {
+    $data = $request->getParsedBody();
+
+    $this->validator = $this->validator->withData($data);
+    if (! $this->validator->validate()) {
+      $response->getBody()->write(json_encode($this->validator->errors()));
+      return $response->withStatus(422);
+    }
+
+    $package = $this->packageService->update((int)$package_id, $data);
+    $response->getBody()->write(json_encode($package));
+    return $response;
+  }
+
+  public function delete(Request $request, Response $response, string $package_id): Response
+  {
+    $package = $this->packageService->delete((int)$package_id);
+    $response->getBody()->write(json_encode($package));
+    return $response;
+  }
+}
---
+++ src/App/Controllers/SubscriptionController.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+use App\Db\Repository\SubscriptionService;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use Valitron\Validator;
+
+class SubscriptionController
+{
+  public function __construct(private SubscriptionService $subscriptionService, private Validator $validator)
+  {
+    $this->validator->mapFieldsRules([
+      'user_id' => ['required', ['lengthMin', 1]],
+      'package_id' => ['required', ['lengthMin', 1]],
+    ]);
+  }
+
+  public function showAll(Request $request, Response $response): Response
+  {
+    $data = $this->subscriptionService->readAll();
+    $response->getBody()->write(json_encode($data));
+    return $response;
+  }
+
+  public function show(Request $request, Response $response, string $subscription_id): Response
+  {
+    $subscription = $request->getAttribute('subscription');
+    $response->getBody()->write(json_encode($subscription));
+    return $response;
+  }
+
+  public function create(Request $request, Response $response): Response
+  {
+    $data = $request->getParsedBody();
+
+    $this->validator = $this->validator->withData($data);
+    if (! $this->validator->validate()) {
+      $response->getBody()->write(json_encode($this->validator->errors()));
+      return $response->withStatus(422);
+    }
+
+    $user = $request->getAttribute('author');
+    $package = $request->getAttribute('package');
+
+    $subscription = $this->subscriptionService->create($user, $package, $data);
+    $response->getBody()->write(json_encode($subscription));
+    return $response;
+  }
+
+  public function update(Request $request, Response $response, string $subscription_id): Response
+  {
+    $data = $request->getParsedBody();
+
+    $this->validator = $this->validator->withData($data);
+    if (! $this->validator->validate()) {
+      $response->getBody()->write(json_encode($this->validator->errors()));
+      return $response->withStatus(422);
+    }
+
+    $subscription = $this->subscriptionService->update((int)$subscription_id, $data);
+    $response->getBody()->write(json_encode($subscription));
+    return $response;
+  }
+
+  public function delete(Request $request, Response $response, string $subscription_id): Response
+  {
+    $subscription = $this->subscriptionService->delete((int)$subscription_id);
+    $response->getBody()->write(json_encode($subscription));
+    return $response;
+  }
+}
