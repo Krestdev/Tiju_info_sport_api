914d8b5d54fdddb5ae190430e2b248588b0653ef
wintercode <kenfackjordanjunior@gmail.com>
2025-03-26 15:50:02 +0100

    add support for x-api-key in CORS headers

--- src/App/Middleware/cors/CorsMiddleware.php
+++ src/App/Middleware/cors/CorsMiddleware.php
@@ -14,6 +14,6 @@ $app->add(function (Request $request, RequestHandler $handler) {
   $response = $handler->handle($request);
   return $response
     ->withHeader('Access-Control-Allow-Origin', '*')
-    ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+    ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization, x-api-key')
     ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 });
