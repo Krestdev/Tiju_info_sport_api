aae768612fdaf029bb9e2fe9fdb1ebe97521bd7e
wintercode <kenfackjordanjunior@gmail.com>
2025-04-08 14:52:23 +0100

    refactor(CorsMiddleware.php): streamline CORS middleware by removing commented code and improving request handling

--- src/App/Middleware/cors/CorsMiddleware.php
+++ src/App/Middleware/cors/CorsMiddleware.php
@@ -2,28 +2,18 @@
 
 declare(strict_types=1);
 
-use Google\Service\Dns\ResponseHeader;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 use Psr\Http\Message\ResponseInterface as Response;
 
-// $app->options('/{routes:.+}', function (Request $request, Response $response, string $args) {
-//   return $response;
-// });
+$app->options('/{routes:.+}', function (Request $request, Response $response, string $args) {
+  return $response;
+});
 
-$app->add(function (Request $request, $handler) {
-  // Bypass for preflight
-  if ($request->getMethod() === 'OPTIONS') {
-    $response = $handler->handle($request);
-    return $response
-      ->withHeader('Access-Control-Allow-Origin', '*')
-      ->withStatus(204);
-  }
-
-  // Normal requests
+$app->add(function (Request $request, RequestHandler $handler) {
   $response = $handler->handle($request);
   return $response
     ->withHeader('Access-Control-Allow-Origin', '*')
-    ->withHeader('Access-Control-Allow-Methods', '*')
-    ->withHeader('Access-Control-Expose-Headers', '*');
+    ->withHeader('Access-Control-Allow-Headers', '*')
+    ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 });
